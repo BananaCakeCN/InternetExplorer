eae4ee88ee3188edeb339924d2703a7a0cc76c33
BananaCakeCN <103292046+BananaCakeCN@users.noreply.github.com>
2023-12-17 15:28:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-for(var i = 0; i < document.getElementsByTagName('input').length; i++){
-    document.getElementsByTagName('input')[i].addEventListener("click", function(){
-        var image = document.createElement('img');
-        image.src = "https://bananacake.top/photos/1.png";
-        image.onload = function(){
-            window.open("imageViewer.html?index=" + i + "&category=" + "photos", "_blank", "width=" + ((image.width/image.height)*500) + "; height=500; left=" + (window.screen.width/2 - ((image.width/image.height)*500)/2) + "; top="+(window.screen.height/2 - 250)+";");
-        }
-    });
+var range = 0;
+var album = photos;
+albumName = 'photos';
+function switchAlbum(name){
+    switch(name) {
+        case 'photos':
+            album = photos;
+            break;
+        case 'screenshots':
+            album = screenshots;
+            break;
+        case 'drawings':
+            album = drawings;
+            break;
+    }
+    albumName = name;
+    range = 0;
+    loadList(range, album);
 }
+function lastPage(){
+    range -= 25;
+    loadList(range, album);
+}
+function nextPage(){
+    range += 25;
+    loadList(range, album);
+}
+function loadList(startIndex, album){
+    document.getElementsByTagName('tbody')[0].innerHTML = '<tr id="top"><th class="table-header-leftmost">&nbsp;</th><th class="table-header"><span>Time</span></th><th class="table-header"><span>Classification</span></th><th class="table-header"><span>View</span></th></tr>';
+    document.getElementById('lengthInfo').innerText = (startIndex + 1) + ' - ' + (startIndex + 25) + ' of ' + Object.keys(album).length + ' (page ' + (startIndex / 25 + 1) + ' of ' + Math.ceil(Object.keys(album).length / 25) + ')';
+    if(startIndex / 25 == 0){
+        document.getElementById('previous').innerHTML = '<img src="button_PreviousArrow_disabled.gif"><span>Previous</span>';
+    }else{
+        document.getElementById('previous').innerHTML = '<img src="button_PreviousArrow.gif"><a href="javascript:lastPage();"><span>Previous</span></a>';
+    }
+    if(startIndex / 25 == Math.ceil(Object.keys(album).length / 25) - 1){
+        document.getElementById('next').innerHTML = '<span>Next</span><img src="button_NextArrow_disabled.gif">';
+    }else{
+        document.getElementById('next').innerHTML = '<a href="javascript:nextPage();"><span>Next</span></a><img src="button_NextArrow.gif">';
+    }
+    for(var i = startIndex; i < startIndex+25 && i < Object.keys(album).length; i++){
+        let block = document.createElement('tr');
+        block.innerHTML = '<tr><td class="table-result"></td><td class="table-result">'+album[Object.keys(album).length-i][1]+'</td><td class="table-result">摄影</td><td class="table-result"><input value="查看" type="button" index="' + (Object.keys(album).length-i) + '"></td></tr>';
+        document.getElementsByTagName('tbody')[0].appendChild(block);
+        document.getElementsByTagName('input')[document.getElementsByTagName('input').length-1].addEventListener("click", function(){
+            var cacheIndex = this.getAttribute('index');
+            var image = document.createElement('img');
+            image.src = "https://bananacake.top/" + albumName + "/" + cacheIndex + ".png";
+            image.onload = function(){
+                window.open("imageViewer.html?index=" + cacheIndex + "&category=" + albumName, "_blank", "width=" + ((image.width/image.height)*500) + "; height=500; left=" + (window.screen.width/2 - ((image.width/image.height)*500)/2) + "; top="+(window.screen.height/2 - 250)+";");
+            }
+        });
+    }
+}
+loadList(range, album);
