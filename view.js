2f1ff08c384c691c1559e9e07c8f650c57fd5059
BananaCakeCN <103292046+BananaCakeCN@users.noreply.github.com>
2023-12-18 11:23:57 +0800

    Update view.js

--- view.js
+++ view.js
@@ -7,4 +7,4 @@ function searchParamsForIE(name){
        return decodeURI(results[1]) || 0;
     }
 }
-document.getElementById("container").innerHTML = '<img style="width: 100%; height: 100%; position: fixed; top: 0; left: 0;" src="https://bananacake.top/'+searchParamsForIE('category')+'/'+searchParamsForIE('index')+'.png">';
+document.getElementById("container").innerHTML = '<img style="width: 100%; height: 100%; position: fixed; top: 0; left: 0;" src="http://assets.bananacake.top/'+searchParamsForIE('category')+'/'+searchParamsForIE('index')+'.png">';
